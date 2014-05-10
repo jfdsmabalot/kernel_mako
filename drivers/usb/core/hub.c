3cc21bf8960182bfb309e64acd0bad3c2a13dece
kiko <jfdsmabalot@gmail.com>
2014-05-10 17:01:09 +0800

    fix linux merge errors Signed-off-by: Jean Francis Dominique S. Mabalot <jfdsmabalot@redhat.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4565,62 +4565,6 @@ re_enumerate:
  */
 int usb_reset_device(struct usb_device *udev)
 {
-        int ret;
-        int i;
-        struct usb_host_config *config = udev->actconfig;
-
-        if (udev->state == USB_STATE_NOTATTACHED ||
-                        udev->state == USB_STATE_SUSPENDED) {
-                dev_dbg(&udev->dev, "device reset not allowed in state %d\n",
-                                udev->state);
-                return -EINVAL;
-        }
-
-        /* Prevent autosuspend during the reset */
-        usb_autoresume_device(udev);
-
-        if (config) {
-                for (i = 0; i < config->desc.bNumInterfaces; ++i) {
-                        struct usb_interface *cintf = config->interface[i];
-                        struct usb_driver *drv;
-                        int unbind = 0;
-
-                        if (cintf->dev.driver) {
-                                drv = to_usb_driver(cintf->dev.driver);
-                                if (drv->pre_reset && drv->post_reset)
-                                        unbind = (drv->pre_reset)(cintf);
-                                else if (cintf->condition ==
-                                                USB_INTERFACE_BOUND)
-                                        unbind = 1;
-                                if (unbind)
-                                        usb_forced_unbind_intf(cintf);
-                        }
-                }
-        }
-
-        ret = usb_reset_and_verify_device(udev);
-
-        if (config) {
-                for (i = config->desc.bNumInterfaces - 1; i >= 0; --i) {
-                        struct usb_interface *cintf = config->interface[i];
-                        struct usb_driver *drv;
-                        int rebind = cintf->needs_binding;
-
-                        if (!rebind && cintf->dev.driver) {
-                                drv = to_usb_driver(cintf->dev.driver);
-                                if (drv->post_reset)
-                                        rebind = (drv->post_reset)(cintf);
-                                else if (cintf->condition ==
-                                                USB_INTERFACE_BOUND)
-                                        rebind = 1;
-                        }
-                        if (ret == 0 && rebind)
-                                usb_rebind_intf(cintf);
-                }
-        }
-
-        usb_autosuspend_device(udev);
-        return ret;
 	int ret;
 	int i;
 	struct usb_host_config *config = udev->actconfig;
